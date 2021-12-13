78e11f3103b64e6e7ab6a3f2bcc495cac96e1722
vitooh <witold.suryjak@gmail.com>
2021-12-13 22:54:00 +0000

    execution time added

--- test_mongo.js
+++ test_mongo.js
@@ -1,12 +1,18 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
+var startTime = performance.now()
+
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("test");
   dbo.collection("hello1").findOne({}, function(err, result) {
     if (err) throw err;
-    console.log(result.x);
+    console.log(result.test);
     db.close();
   });
 });
+
+var endTime = performance.now()
+
+console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
--- test_mysql.js
+++ test_mysql.js
@@ -6,11 +6,18 @@ var con = mysql.createConnection({
   password: "password",
   database: "test"
 });
+var startTime = performance.now()
 
 con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT * FROM hello1", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
+  con.end(function(err) {
+  // The connection is terminated now
+  });
   });
 });
+var endTime = performance.now()
+
+console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
